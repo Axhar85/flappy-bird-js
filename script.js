98aa93756d03406bb20f81ed057535b34efc7b44
Axhar85 <azhararshad_ntu@yahoo.com>
2021-09-23 18:33:31 +0200

    added code if you hit you lose and its ready to play

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 var charcter = document.getElementById("charcter");
 var block = document.getElementById("block");
 function jump(){
-    
+    if(character.classList != "animate"){ 
     character.classList.add("animate")
+    }
     setTimeout(function(){
         character.classList.remove("animate")
     },500);
 }
+
+// check if chracter is dead or not set 10s interval of time
+var checkDead = setInterval(function(){
+    var characterTop = parseInt
+    (window.getComputedStyle(character).getPropertyValue("top"));
+
+    var blockLeft = parseInt
+    (window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>0 && characterTop >=130){
+        block.style.animation = "none";
+        block.style.display = "none";
+        alert("you lose");
+    }
+}, 10);
